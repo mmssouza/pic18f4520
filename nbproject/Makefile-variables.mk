e57574c033e8988344bd067c3b7c2e1cc4f08a51
Marcelo M S Souza <marcelo@ufc.br>
2025-10-29 21:11:56 -0300

    Squashed 'exemplo_adc_pwm_motor.X/' content from commit 056fcd8
    git-subtree-dir: exemplo_adc_pwm_motor.X
    git-subtree-split: 056fcd8be8c3cc9eabb193da5392c87c1850e11d

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=exemplo_adc_pwm_motor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/exemplo_adc_pwm_motor.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=exemploadcpwmmotor.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/exemploadcpwmmotor.x.tar
