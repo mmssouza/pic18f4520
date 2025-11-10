cbaa9501c96a6439a653690a25def46dc620009a
@m4rkn4la <13985581+AlanMarquesRocha@users.noreply.github.com>
2025-11-10 09:18:42 -0300

    Add files
    Add files

---
+++ Práticas/Prática 04 - Controle de ventoinha/P04_controle_de_ventoinha.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=P04_controle_de_ventoinha.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/P04_controle_de_ventoinha.X.production.hex
